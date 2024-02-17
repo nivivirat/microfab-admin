afc6803c7ef89fbd1d662d333180eae83689686d
nivivirat <imniveditha1188@gmail.com>
2024-02-17 19:36:44 +0530

    key figures done

--- src/Components/AboutUs/AboutUsComponetns/KeyFigures/KeyFigures.jsx
+++ src/Components/AboutUs/AboutUsComponetns/KeyFigures/KeyFigures.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import CountUp from 'react-countup';
 import { db, storage } from '../../../../../firebase';
+import figure from '../../../../assets/AboutUs/KeyFigures/keyFigures.svg'
 import { ref, get, update } from 'firebase/database';
-import { getDownloadURL, uploadBytes, getStorage, ref as storageRef } from 'firebase/storage';
+import { getDownloadURL, uploadBytes, ref as storageRef } from 'firebase/storage';
 
 const KeyFigures = () => {
     const [data, setData] = useState(null);
@@ -52,14 +53,13 @@ const KeyFigures = () => {
 
     const handleInputChange = (event, blockIndex, boxIndex, field) => {
         const newData = { ...data };
-        newData.blocks[blockIndex].boxes[boxIndex][field] = event.target.value;
-        setData(newData);
-    };
+        const inputValue = event.target.value;
 
-    const handleImageChange = (event) => {
-        if (event.target.files[0]) {
-            setNewImage(event.target.files[0]);
-        }
+        // Check if the input value is a number
+        // const isNumber = !isNaN(inputValue);
+
+        newData.blocks[blockIndex].boxes[boxIndex][field] = inputValue;
+        setData(newData);
     };
 
     if (loading) {
@@ -70,25 +70,27 @@ const KeyFigures = () => {
         return <p>Error: {error}</p>;
     }
 
+    const handleCancelClick = () => {
+        setEditMode(false);
+    };
+
     return (
         <div className='px-10 border border-primary rounded-lg p-10 flex-row'>
+            <div className='pl-10'>
+                <ul className='list-disc'>
+                    <li>place only numbers in the left box to get counting animation</li>
+                </ul>
+            </div>
             {data && (
                 <div className="flex mt-12 h-[600px] animate__animated animate__fadeInLeft animate__delay-2s">
                     <div className="bg-white md:mb-32 mb-44">
-                        {editMode ? (
-                            <div className='relative'>
-                                <img src={data.blocks[0].imageUrl} alt={data.blocks[0].imageAlt} className="w-[650px] md:block hidden h-full object-cover" />
-                                <input type="file" accept="image/*" onChange={handleImageChange} className='absolute top-0'/>
-                            </div>
-                        ) : (
-                            <img src={data.blocks[0].imageUrl} alt={data.blocks[0].imageAlt} className="w-[650px] md:block hidden h-full object-cover" />
-                        )}
+                        <img src={figure} alt={data.blocks[0].imageAlt} className="w-[650px] md:block hidden h-full object-cover" />
                     </div>
                     <div className="flex-1 mb-24 sm:ml-0 bg-white p-8 flex flex-col place-items-center justify-center">
                         <h3 className="lg:ml-0 sm:ml-0 text-5xl sm:text-4xl text-[#8AA6AA] pb-5 leading-tight font-['ClashDisplay']">
                             <strong>
                                 {editMode ? (
-                                    <input type="text" value={data.title} onChange={(e) => handleInputChange(e, 0, 0, 'title')} className="border border-white bg-primary text-white px-2 py-1 rounded-md"/>
+                                    <input type="text" value={data.title} onChange={(e) => handleInputChange(e, 0, 0, 'title')} className="border border-white bg-primary text-white px-2 py-1 rounded-md" />
                                 ) : (
                                     data.title
                                 )}
@@ -96,7 +98,7 @@ const KeyFigures = () => {
                         </h3>
                         <h3 className="mb-4 text-2xs text-[#8AA6AA] mx-10 leading-tight font-['ClashDisplay']">
                             {editMode ? (
-                                <textarea value={data.description} onChange={(e) => handleInputChange(e, 0, 0, 'description')} className="border border-white bg-primary text-white px-2 py-1 rounded-md"/>
+                                <textarea value={data.description} onChange={(e) => handleInputChange(e, 0, 0, 'description')} className="border border-white bg-primary w-[500px] text-white px-2 py-1 rounded-md" />
                             ) : (
                                 data.description
                             )}
@@ -109,11 +111,24 @@ const KeyFigures = () => {
                                             <center>
                                                 <strong>
                                                     {editMode ? (
-                                                        <input type="number" value={box.count} onChange={(e) => handleInputChange(e, 1, boxIndex, 'count')} className="border border-white bg-primary text-white px-2 py-1 rounded-md"/>
+                                                        <>
+                                                            <span className='flex flex-row border border-black'>
+                                                                <input type="number" value={box.count} onChange={(e) => handleInputChange(e, 1, boxIndex, 'count')} className="border border-white bg-primary text-white px-2 py-1 w-[70px] text-sm rounded-md mr-1" />
+                                                                <input type="text" value={box.customValue} onChange={(e) => handleInputChange(e, 1, boxIndex, 'customValue')} className="border border-white bg-primary text-white px-2 w-[30px] py-1 rounded-md" />
+                                                            </span>
+                                                            <input type="text" value={box.percentage} onChange={(e) => handleInputChange(e, 1, boxIndex, 'percentage')} className="border border-white bg-primary text-white px-2 w-[100px] py-1 rounded-md mt-1" />
+                                                        </>
                                                     ) : (
-                                                        <CountUp end={box.count} duration={box.countDuration} separator="," />
+                                                        <>
+                                                            <CountUp end={box.count} duration={box.countDuration} separator="," />
+                                                            {box.customValue && box.customValue}
+                                                            {/* <br /> */}
+                                                            <br />
+                                                            <span className='text-2xs'>
+                                                                {box.percentage && box.percentage}
+                                                            </span>
+                                                        </>
                                                     )}
-                                                    {box.percentage && box.percentage}
                                                 </strong>
                                             </center>
                                         </h3>
@@ -121,7 +136,7 @@ const KeyFigures = () => {
                                         <h3 className=" text-xs text-white w-full m-0 leading-tight ll font-['ClashDisplay']">
                                             <center>
                                                 {editMode ? (
-                                                    <input type="text" value={box.title} onChange={(e) => handleInputChange(e, 1, boxIndex, 'title')} className="border border-white bg-primary text-white px-2 py-1 rounded-md"/>
+                                                    <input type="text" value={box.title} onChange={(e) => handleInputChange(e, 1, boxIndex, 'title')} className="border w-[120px] border-white bg-primary text-white px-2 py-1 rounded-md" />
                                                 ) : (
                                                     box.title
                                                 )}
@@ -133,9 +148,14 @@ const KeyFigures = () => {
                         </div>
                         <div className="mt-4">
                             {editMode ? (
-                                <button onClick={handleSaveClick} className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                                    Save
-                                </button>
+                                <>
+                                    <button onClick={handleSaveClick} className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">
+                                        Save
+                                    </button>
+                                    <button onClick={handleCancelClick} className="bg-gray-500 text-white px-4 py-2 rounded-md">
+                                        Cancel
+                                    </button>
+                                </>
                             ) : (
                                 <button onClick={handleEditClick} className="bg-green-500 text-white px-4 py-2 rounded-md">
                                     Edit
